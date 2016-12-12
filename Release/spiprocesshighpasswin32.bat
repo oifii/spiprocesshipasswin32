d5b6fa57a6ba4f0401b1e9c5f1a4fae38f7eb187
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 08:50:26 -0500

    first commit

---
+++ Release/spiprocesshighpasswin32.bat
@@ -0,0 +1,3 @@
+rem start spiprocesshighpasswin32.exe "E-MU ASIO" 0 1 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spiprocesshighpasswin32.exe "E-MU ASIO" 4 5 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+start spiprocesshighpasswin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 1000.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
